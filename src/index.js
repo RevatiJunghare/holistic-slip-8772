9c35c4bc94f1cf9f400ed11a9be5d4a14eaab9a6
janani <jananijairam27@gmail.com>
2022-11-10 23:26:27 +0530

    day-3

--- src/index.js
+++ src/index.js
@@ -106,29 +106,196 @@ let bestSeller = (bestSellersLeftArr,container)=>{
 
 
 bestSeller(bestSellersLeftArr,pro_div)
-let flag1= true;
+// let flag1= true;
 
 
-document.querySelector(".left_arr_div").addEventListener("click",()=>{
+// document.querySelector(".left_arr_div").addEventListener("click",()=>{
 
-    flag1=!flag1
-    if(flag1){
-        bestSeller(bestSellersLeftArr,pro_div)
-    }else{
-        bestSeller(bestSellersRightArr,pro_div)
-    }
+//     flag1=!flag1
+//     if(flag1){
+//         bestSeller(bestSellersLeftArr,pro_div)
+//     }else{
+//         bestSeller(bestSellersRightArr,pro_div)
+//     }
     
-})
-let flag2 = false
-document.querySelector(".right_arr_div").addEventListener("click",()=>{
-    flag2 = !flag2
-    if(flag2){
-        bestSeller(bestSellersRightArr,pro_div)
-    }else{
-        bestSeller(bestSellersLeftArr,pro_div)
-    }
+// })
+// let flag2 = false
+// document.querySelector(".right_arr_div").addEventListener("click",()=>{
+//     flag2 = !flag2
+//     if(flag2){
+//         bestSeller(bestSellersRightArr,pro_div)
+//     }else{
+//         bestSeller(bestSellersLeftArr,pro_div)
+//     }
     
-})
+// })
 
 
+let videoPlayArr = [
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/3/9/1646836045-uerkdvpa/watermarked/540/InShot_20220309_1844460631.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/2/15/1644919919-drjtghip/watermarked/540/EasyEyelinerHackusingaSpoon.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/1/18/1642490662-qrlyukpb/watermarked/540/HowtoBaketheRightWay.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/4/30/1651342203-wnqistjz/watermarked/540/EffortlesslyExpressive9-16.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/1/18/1642490844-ureajzhm/watermarked/540/FaceLiftlikeKendallJenner.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/2/15/1644906634-nshmulgo/watermarked/540/FoundationHacks.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/2/15/1644905324-hgvtazeo/watermarked/540/KoreanBeautyHack-JAMSU.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/2/4/1643956359-clqnsktd/watermarked/540/SunkissedLookTutorial.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/1/18/1642491110-kgvyhwbt/watermarked/540/ReverseCatEyeliner.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/3/9/1646836364-eixsrdot/watermarked/540/275182554_2096939307119931_4496228888503996313_n.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/3/9/1646835858-lvtrfujp/watermarked/540/InShot_20220309_184811595.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/2/4/1643955080-zfievyxo/watermarked/540/TissuePaperEyeshadowHack.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2021/11/1/1635767024-pedhjtzf/watermarked/540/EasywaytoMattifyLipstick7.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/3/9/1646836196-scobeuaq/watermarked/540/Flawlessrecreationoftheiconicdiva.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/3/9/1646836728-bkvwtnrc/watermarked/540/InShot_20220309_183351048.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/3/9/1646836997-qhfletip/watermarked/540/GlamSmokeyEyelook.mp4",
+    },
+    {
+      src: "https://cdn4.fireworktv.com/medias/2022/4/22/1650623566-berqzfil/watermarked/540/MettlePrimingBalm-Vertical.mp4",
+    },
+  ];
 
+let vid_div = document.querySelector(".vidSlider");
+let videoPlay = (data) => {
+  data.forEach((el) => {
+    let video_div = document.createElement("div");
+    video_div.className = "video_div";
+    let video = document.createElement("video");
+    video.src = el.src;
+    video.style.border = "0";
+    video.className = "videoshorts";
+    video.style.cursor = "pointer";
+     console.log(video)
+    vid_div.append(video);
+  });
+};
+videoPlay(videoPlayArr);
+const clip = document.querySelectorAll(".videoshorts");
+for (let i = 0; i < clip.length; i++) {
+  clip[i].addEventListener("mouseover", (e) => {
+    clip[i].play();
+  });
+  clip[i].addEventListener("mouseout", (e) => {
+    clip[i].pause();
+  });
+}
+let vidTemp = 0;
+document.querySelector("#left").addEventListener("click", () => {
+  let winWidth = window.outerWidth;
+  vidTemp = vidTemp - winWidth * 0.74;
+
+  if (vidTemp > 0) {
+    console.group("left", vidTemp);
+    vid_div.style.transform = `translateX(${-vidTemp}px)`;
+    vid_div.style.transition = ".4s";
+  } else {
+    vid_div.style.transform = `translateX(${-0}px)`;
+    vid_div.style.transition = ".4s";
+    vidTemp = 0;
+  }
+});
+document.querySelector("#right").addEventListener("click", () => {
+  let winWidth = window.outerWidth;
+
+  vidTemp = vidTemp + winWidth * 0.74;
+  console.group("right", vidTemp, winWidth);
+  if (vidTemp < winWidth * 3) {
+    vid_div.style.transform = `translateX(${-vidTemp}px)`;
+    vid_div.style.transition = ".4s";
+  } else {
+    vidTemp = vidTemp - winWidth * 0.74;
+  }
+});
+
+function slideShow2(){
+    const arr=['https://d32baadbbpueqt.cloudfront.net/Homepage/a1ea3283-9593-4f1c-8e6b-70c3a5062c37.jpg',
+'https://d32baadbbpueqt.cloudfront.net/Homepage/37471559-27d1-4509-9e31-29d889be7036.jpg',
+'https://d32baadbbpueqt.cloudfront.net/Homepage/f190c764-84cf-4eea-a381-8b701be65852.gif',
+'https://d32baadbbpueqt.cloudfront.net/Homepage/89e51326-1363-44e1-94f1-d5bd00bd2ca9.gif',
+'https://d32baadbbpueqt.cloudfront.net/Homepage/6985f902-c3fe-4a20-bc76-1658a9109fd1.jpg',
+'https://d32baadbbpueqt.cloudfront.net/Homepage/730cf92b-3d4a-4d08-bed8-db88e7911f32.jpg']
+
+    let i=0;
+    let div=document.getElementById('carousel2')
+    
+    let img=document.createElement('img');
+    img.src=arr[0];
+
+    div.append(img);
+    i=i+1;
+    
+    setInterval(function(){
+
+        if(i==6){
+            i=0;
+        }
+        img.src=arr[i];
+        i=i+1;
+
+        div.append(img);
+
+    }, 3000);
+}
+
+slideShow2();
+
+// function slideShow3(){
+//     const arr=[
+// 'https://d32baadbbpueqt.cloudfront.net/Homepage/7fcf7da7-9d0b-4e1a-b09e-e075825bc9ad.jpg',
+// 'https://d32baadbbpueqt.cloudfront.net/Homepage/05b6a9d3-2d45-4381-80f2-2b3fdd2c56f0.jpg',
+// 'https://d32baadbbpueqt.cloudfront.net/Homepage/67885afc-5ba2-4f1d-a6dd-7012bf5b7b90.jpg'
+// ]
+
+// let i=0;
+// let div=document.getElementById('carousel3')
+
+// let img=document.createElement('img');
+// img.src=arr[0];
+
+// div.append(img);
+// i=i+1;
+
+// setInterval(function(){
+
+//     if(i==3){
+//         i=0;
+//     }
+//     img.src=arr[i];
+//     i=i+1;
+
+//     div.append(img);
+
+// }, 2000);
+// }
+
+// slideShow3();
